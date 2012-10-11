44dfd2b8e6e63530a34b6dcb8b6be192937a9881
mikhail.kotelnikov <mikhail.kotelnikov@webreformatter.com>
2012-10-11 02:26:34 +0200

    getMap(...) and getList(...) methods were introduced.

--- src/main/java/org/ubimix/commons/parser/json/utils/JavaObjectVisitor.java
+++ src/main/java/org/ubimix/commons/parser/json/utils/JavaObjectVisitor.java
@@ -35,7 +35,21 @@ public class JavaObjectVisitor extends AbstractObjectVisitor {
     };
 
     private static String keyToString(Object o1) {
-        return o1 != null ? o1.toString() : "<null>";
+        return o1 != null ? o1.toString() : "";
+    }
+
+    protected Iterable<?> getList(Object value) {
+        if (value instanceof Iterable<?>) {
+            return (Iterable<?>) value;
+        }
+        return null;
+    }
+
+    protected Map<?, ?> getMap(Object value) {
+        if (value instanceof Map<?, ?>) {
+            return (Map<?, ?>) value;
+        }
+        return null;
     }
 
     /**
@@ -54,69 +68,92 @@ public class JavaObjectVisitor extends AbstractObjectVisitor {
         if (value == null && !acceptNull) {
             return;
         }
+        if (stack.contains(value)) {
+            return;
+        }
+        stack.add(value);
         try {
+            boolean result = false;
             if (value == null) {
                 listener.onValue(null);
-            } else if (value instanceof Map<?, ?>) {
-                listener.beginObject();
-                if (sort) {
-                    Map<?, ?> map = (Map<?, ?>) value;
-                    List<Object> keys = new ArrayList<Object>(map.keySet());
-                    Collections.sort(keys, KEY_COMPARATOR);
-                    for (Object key : keys) {
-                        String name = keyToString(key);
-                        listener.beginObjectProperty(name);
-                        Object mapValue = map.get(key);
-                        visit(mapValue, sort, listener, stack, true);
-                        listener.endObjectProperty(name);
+                result = true;
+            }
+            if (!result) {
+                Map<?, ?> map = getMap(value);
+                if (map != null) {
+                    listener.beginObject();
+                    if (sort) {
+                        List<Object> keys = new ArrayList<Object>(map.keySet());
+                        Collections.sort(keys, KEY_COMPARATOR);
+                        for (Object key : keys) {
+                            String name = keyToString(key);
+                            listener.beginObjectProperty(name);
+                            Object mapValue = map.get(key);
+                            visit(mapValue, sort, listener, stack, true);
+                            listener.endObjectProperty(name);
+                        }
+                    } else {
+                        for (Map.Entry<?, ?> entry : map.entrySet()) {
+                            String name = keyToString(entry.getKey());
+                            listener.beginObjectProperty(name);
+                            Object mapValue = entry.getValue();
+                            visit(mapValue, sort, listener, stack, true);
+                            listener.endObjectProperty(name);
+                        }
                     }
-                } else {
-                    for (Map.Entry<?, ?> entry : ((Map<?, ?>) value).entrySet()) {
-                        String name = keyToString(entry.getKey());
-                        listener.beginObjectProperty(name);
-                        Object mapValue = entry.getValue();
-                        visit(mapValue, sort, listener, stack, true);
-                        listener.endObjectProperty(name);
+                    listener.endObject();
+                    result = true;
+                }
+                if (!result) {
+                    Iterable<?> list = getList(value);
+                    if (list != null) {
+                        listener.beginArray();
+                        for (Object o : list) {
+                            listener.beginArrayElement();
+                            visit(o, sort, listener, stack, true);
+                            listener.endArrayElement();
+                        }
+                        listener.endArray();
+                        result = true;
                     }
                 }
-                listener.endObject();
-            } else if (value instanceof Iterable<?>) {
-                listener.beginArray();
-                for (Object o : (Iterable<?>) value) {
-                    listener.beginArrayElement();
-                    visit(o, sort, listener, stack, true);
-                    listener.endArrayElement();
+                if (!result) {
+                    if (value.getClass().isArray()) {
+                        listener.beginArray();
+                        for (Object o : (Object[]) value) {
+                            listener.beginArrayElement();
+                            visit(o, sort, listener, stack, true);
+                            listener.endArrayElement();
+                        }
+                        listener.endArray();
+                        result = true;
+                    }
                 }
-                listener.endArray();
-            } else if (value.getClass().isArray()) {
-                listener.beginArray();
-                for (Object o : (Object[]) value) {
-                    listener.beginArrayElement();
-                    visit(o, sort, listener, stack, true);
-                    listener.endArrayElement();
+
+                if (!result) {
+                    if (value instanceof Integer) {
+                        listener.onValue((Integer) value);
+                    } else if (value instanceof Long) {
+                        listener.onValue((Long) value);
+                    } else if (value instanceof Short) {
+                        listener.onValue((Short) value);
+                    } else if (value instanceof Character) {
+                        listener.onValue((Character) value);
+                    } else if (value instanceof Byte) {
+                        listener.onValue((Byte) value);
+                    } else if (value instanceof Double) {
+                        listener.onValue((Double) value);
+                    } else if (value instanceof Float) {
+                        listener.onValue((Float) value);
+                    } else if (value instanceof Boolean) {
+                        listener.onValue((Boolean) value);
+                    } else if (value instanceof String) {
+                        listener.onValue((String) value);
+                    } else {
+                        String str = value != null ? value.toString() : null;
+                        listener.onValue(str);
+                    }
                 }
-                listener.endArray();
-            } else if (value instanceof Integer) {
-                listener.onValue((Integer) value);
-            } else if (value instanceof Long) {
-                listener.onValue((Long) value);
-            } else if (value instanceof Short) {
-                listener.onValue((Short) value);
-            } else if (value instanceof Character) {
-                listener.onValue((Character) value);
-            } else if (value instanceof Byte) {
-                listener.onValue((Byte) value);
-            } else if (value instanceof Double) {
-                listener.onValue((Double) value);
-            } else if (value instanceof Float) {
-                listener.onValue((Float) value);
-            } else if (value instanceof Boolean) {
-                listener.onValue((Boolean) value);
-            } else if (value instanceof String) {
-                listener.onValue((String) value);
-            } else {
-                String str = value != null ? value.toString() : null;
-                listener.onValue(str);
             }
         } finally {
             stack.remove(value);
